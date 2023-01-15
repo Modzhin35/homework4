114a663fe3b5bf6e3dfda3ba5ee8743d02bbe7c5
modzhin <modzhds@gmail.com>
2023-01-15 23:40:32 +0300

    Task29

---
+++ task29/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+void ContPrintArr (int length){
+    int [] arr = new int [Math.Abs(length)];
+    for(int i=0; i<Math.Abs(length); i++) arr[i]=new Random().Next(-99, 99);
+    for (int j=0; j<Math.Abs(length); j++) Console.Write($"{arr[j]} ");
+}
+
+Console.Write("Введите размерность массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+ContPrintArr(size);
